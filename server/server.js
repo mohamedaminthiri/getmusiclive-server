1c49e03c2cacb07860aa905f1e5e3afa7d42f95c
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-03-20 21:56:34 -0700

    handle critical dependency warning

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
+import data from '../data/data.json';
 
 const app = express();
 const PORT = process.env.NODE_ENV || 5000;
 
 app.get('*', (req, res) => {
-  res.send('working');
+  res.send(data);
 });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-})
+});
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,11 +1,22 @@
 const { resolve } = require('path');
+const { readdirSync } = require('fs');
+
+const nodeModules = {};
+
+readdirSync('node_modules')
+  .filter(x => ['.bin'].indexOf(x) === -1)
+  .forEach(mod => nodeModules[mod] = 'commonjs ' + mod);
 
 module.exports = {
   context: resolve(__dirname, 'server/'),
   target: 'node',
+  node: {
+    fs: 'empty'
+  },
   entry: './server.js',
   output: {
     path: 'dist',
     filename: 'server.bundle.js'
-  }
+  },
+  externals: nodeModules
 };
