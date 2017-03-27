af316d9e5da2f9055b694f7764b16baa0e2cb55e
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-03-27 02:26:40 -0700

    close #7 created a route to the events table

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 import data from '../data/data.json';
 
 const app = express();
@@ -10,6 +11,18 @@ app.get('/', (req, res) => {
 
 app.get('/api', (req, res) => {
   res.type('json').send(data);
+});
+
+app.get('/events', (req, res) => {
+  console.log('Call to events');
+  axios.get('http://localhost:3350/events')
+    .then(data => {
+      console.log(data.data[0]);
+      res.type('json').send(data.data);
+      
+    }).catch(err => {
+      if (err) console.error(err);
+    })
 });
 
 app.listen(PORT, () => {
