bf07ef6b6d4c06429afc44ad550f69514bab22d9
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-04-09 21:18:39 -0700

    setup Docker Compose close #12

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 import data from '../data/data.json';
 import { Observable } from 'rxjs';
 
-console.log(Observable);
-
 const app = express();
 const PORT = process.env.NODE_ENV || 5000;
 
@@ -25,6 +23,43 @@ app.get('/api', (req, res) => {
 //       if (err) console.error(err);
 //     })
 // });
+
+app.get('/events', (req, res, next) => {
+  const getEvents = Observable
+    .fromPromise(axios.get('http://localhost:3350/events'));
+  
+  const subScr = getEvents.subscribe({
+    next: (x) => {
+      // res.send(x);
+      // console.log((x && x.data) || {});
+      // console.log(Object.keys(x.da || {}));
+      // next();
+      res.send((x && x.data) || {});
+      return x;
+    },
+    
+    error: (err) => {
+      next(err);
+      console.log(err);
+    },
+    
+    completed: () => {
+      console.log('completed');
+    }
+  });
+  
+});
+
+// 
+// app.get('/events', (req, res) => {
+//   const getEvents = Observable
+//     .fromPromise(axios.get('http://localhost:3350/events'));
+//   
+//   const subScr = getEvents.subscribe(val => res.send(val));
+//   
+//   subScr.next();
+// });
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
